81ba0d4cad962de4233593da23c634e8984feb7a
pekerleke <mrtnsalem@gmail.com>
2024-03-09 17:40:12 -0300

    add config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    basePath: "/bad-ui",
+};
 
 export default nextConfig;
